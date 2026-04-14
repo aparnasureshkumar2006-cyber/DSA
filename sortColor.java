154ea405b5e674b87b61f69df729de2701862ff2
Aparna <aparnasureshkumar2006@gmail.com>
2026-04-14 14:57:28 +0530

    Add files via upload

---
+++ sortColor.java
@@ -0,0 +1,13 @@
+public class sortColor {
+    public void sortColors(int[] nums) {
+        for(int i = 0;i<nums.length-1;i++){
+            for(int j = i;j<nums.length;j++){
+                if(nums[i] > nums[j]){
+                    int temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+                }
+            }
+        }
+    }
+}
