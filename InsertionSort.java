51acda9ff9057bed47fbc43c2e7da75e6ca4e11e
Aparna <aparnasureshkumar2006@gmail.com>
2026-04-14 15:00:43 +0530

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,13 @@
+public class InsertionSort {
+    public void insertionSort(int arr[]) {
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=i;j<arr.length;j++){
+                if(arr[i] >= arr[j]){
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+    }
+}
