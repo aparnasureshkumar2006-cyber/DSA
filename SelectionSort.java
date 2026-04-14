f5848444e4ba57ce1793f62d329c4551f5e89938
Aparna <aparnasureshkumar2006@gmail.com>
2026-04-14 14:52:10 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,13 @@
+public class SelectionSort {
+    void selectionSort(int[] arr) {
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i] > arr[j]){
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+    }
+}
