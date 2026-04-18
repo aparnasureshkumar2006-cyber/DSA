9a8e87663dcc4efa7552af77c46b7b10ef7c15cf
Aparna <aparnasureshkumar2006@gmail.com>
2026-04-18 23:06:44 +0530

    Search in Rotated Sorted Array - 2

--- SearchRotateSortedArrayTwo.java
+++ SearchRotateSortedArrayTwo.java
@@ -1,2 +1,10 @@
 public class SearchRotateSortedArrayTwo {
+    public boolean search(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == target){
+                return true;
+            }
+        }
+        return false;
+    }
 }
