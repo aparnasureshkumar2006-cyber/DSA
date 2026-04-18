68c3553025f071ceebb7b130a84c0667a8d70d40
Aparna <aparnasureshkumar2006@gmail.com>
2026-04-18 23:34:45 +0530

    Sort the Array of 0's ,1's and 2's

---
+++ SortArray.java
@@ -0,0 +1,13 @@
+public class SortArray {
+    public void sort012(int[] arr) {
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i] > arr[j]){
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+    }
+}
