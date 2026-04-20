b574f3e2d36f8a1239891d6bc23dd5e03e312f68
Aparna <aparnasureshkumar2006@gmail.com>
2026-04-20 12:22:57 +0530

    Binary Search

---
+++ BinarySearch.java
@@ -0,0 +1,10 @@
+public class BinarySearch {
+    static boolean searchInSorted(int arr[], int k) {
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] == k){
+                return true;
+            }
+        }
+        return false;
+    }
+}
