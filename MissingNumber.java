ed7d835950092d5e628a1d38945f9338dc8ea83f
Aparna <aparnasureshkumar2006@gmail.com>
2026-06-20 23:06:30 +0530

    Add files via upload

--- MissingNumber.java
+++ MissingNumber.java
@@ -3,13 +3,14 @@ import java.util.HashSet;
 public class MissingNumber {
     public int missingNumber(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
-        for(int num : nums){
+        for (int num : nums) {
             set.add(num);
         }
-        for(int i =0;i<=nums.length;i++){
-            if(!set.contains(i)){
+        for (int i = 0; i <= nums.length; i++) {
+            if (!set.contains(i)) {
                 return i;
             }
         }
         return -1;
+    }
 }
